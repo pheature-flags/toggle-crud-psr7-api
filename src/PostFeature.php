b959b4307d2cea5d92f8a3b47f946c2e0d3fa2c2
Koldo Picaza <dev@antidotfw.io>
2021-04-12 20:52:44 +0200

    test post feature

--- src/PostFeature.php
+++ src/PostFeature.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Pheature\Crud\Psr7\Toggle;
 
+use InvalidArgumentException;
 use Pheature\Crud\Toggle\Command\CreateFeature as CreateFeatureCommand;
 use Pheature\Crud\Toggle\Handler\CreateFeature;
 use Psr\Http\Message\ResponseFactoryInterface;
@@ -26,12 +27,17 @@ final class PostFeature implements RequestHandlerInterface
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $featureId = $request->getAttribute('feature_id');
-        Assert::string($featureId);
+
+        try {
+            Assert::string($featureId);
+        } catch (InvalidArgumentException $exception) {
+            return $this->responseFactory->createResponse(404, 'Route Not Found.');
+        }
 
         $this->createFeature->handle(
             CreateFeatureCommand::disabled($featureId)
         );
 
-        return $this->responseFactory->createResponse(201, 'Created');
+        return $this->responseFactory->createResponse(201, 'Created.');
     }
 }
